0987692c2cb4e8ad0e888cf0c31980f1239d317f
bigfatcat <bigfatcat0725@gmail.com>
2022-03-06 13:00:54 +0900

    Initial commit

---
+++ lib/components/profile_tab.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:cached_network_image/cached_network_image.dart';
+
+class ProfileTab extends StatefulWidget {
+  const ProfileTab({Key? key}) : super(key: key);
+
+  @override
+  _ProfileTabState createState() => _ProfileTabState();
+}
+
+class _ProfileTabState extends State<ProfileTab>
+    with SingleTickerProviderStateMixin {
+  TabController? _tabController;
+
+  @override
+  void initState() {
+    super.initState();
+    _tabController = new TabController(length: 2, vsync: this);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        _buildTabBar(),
+        Expanded(child: _buildTabBarView()),
+      ],
+    );
+  }
+
+  Widget _buildTabBar() {
+    return TabBar(
+      controller: _tabController,
+      tabs: [
+        Tab(
+            icon: Icon(
+          Icons.directions_car,
+        )),
+        Tab(
+            icon: Icon(
+          Icons.directions_transit,
+        )),
+      ],
+    );
+  }
+
+  Widget _buildTabBarView() {
+    return TabBarView(
+      controller: _tabController,
+      children: [
+        GridView.builder(
+          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+            crossAxisSpacing: 10,
+            crossAxisCount: 3,
+            mainAxisSpacing: 10,
+          ),
+          itemCount: 42,
+          itemBuilder: (context, index) {
+            return Image(
+              image: CachedNetworkImageProvider(
+                  "https://picsum.photos/id/${index + 1}/200/200"),
+            );
+          },
+        ),
+        Container(color: Colors.red)
+      ],
+    );
+  }
+}
